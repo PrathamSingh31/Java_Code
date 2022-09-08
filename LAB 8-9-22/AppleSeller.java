7af1934d6bb10c247a4c24d9125b67bda167d92a
PrathamSingh31 <113005988+PrathamSingh31@users.noreply.github.com>
2022-09-08 14:58:26 +0530

    Add files via upload

---
+++ LAB 8-9-22/AppleSeller.java
@@ -0,0 +1,12 @@
+class AppleSeller
+{
+public static void main(String[] args)
+{
+
+System.out.println(" No of apple sold is 40%");
+int x = (100-40);
+System.out.println( "The number of apple remaining is 420 ");
+int apple = 420*100/x;
+System.out.println("Number of apple he has is " + apple);
+}
+}
