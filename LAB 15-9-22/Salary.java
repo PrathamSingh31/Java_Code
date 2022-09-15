42c4fb888b3c2a785b14db51f05466ebcf5ad00b
PrathamSingh31 <113005988+PrathamSingh31@users.noreply.github.com>
2022-09-15 15:27:12 +0530

    Add files via upload

---
+++ LAB 15-9-22/Salary.java
@@ -0,0 +1,18 @@
+class Salary
+{
+public static void main(String[] args)
+  {
+
+    int average = 10000;
+
+    int employee1 = 11000 , employee2=5000;
+
+    System.out.println("The average salary of employee = " + 3*average);
+
+    int employee3 = 3*average-employee1-employee2;
+
+    System.out.println("The salary of third employee is " + employee3);
+
+  }
+
+}
